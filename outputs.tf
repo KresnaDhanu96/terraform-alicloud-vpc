00af1b959f90411af998a337c4afcafffd169668
KresnaDhanu96 <kresna.dhanumukti@gmail.com>
2020-06-23 17:00:30 +0700

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,40 @@
+# VPC
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = module.vpc.this_vpc_id
+}
+
+output "vpc_cidr_block" {
+  description = "The VPC cidr block"
+  value       = module.vpc.this_vpc_cidr_block
+}
+
+output "vpc_tags" {
+  description = "The tags of the VPC"
+  value       = module.vpc.this_vpc_tags
+}
+
+output "vpc_name" {
+  description = "The name of the VPC"
+  value       = module.vpc.this_vpc_name
+}
+
+# Subnets
+output "vswitch_ids" {
+  description = "List of IDs of vswitch"
+  value       = module.vpc.this_vswitch_ids
+}
+output "vswitch_tags" {
+  description = "List of IDs of vswitch"
+  value       = module.vpc.this_vswitch_tags
+}
+
+output "vswitch_cidr_block" {
+  description = "The vswitch cidr block"
+  value       = module.vpc.this_vswitch_cidr_blocks
+}
+
+output "vswitch_name" {
+  description = "The name of vswitch"
+  value       = module.vpc.this_vswitch_names
+}
